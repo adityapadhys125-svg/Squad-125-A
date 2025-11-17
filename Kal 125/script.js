9a61fe64a09f9e79e11e9d3c9b8140b3f706c2e9
adityapadhys125-svg <aditya.padhy.s.125@kalvium.community>
2025-11-17 12:20:29 +0530

    ok

--- Kal 125/script.js
+++ Kal 125/script.js
@@ -1,1 +1,18 @@
 console.log("Page Loaded");
+console.log("Page Loaded");
+
+// ---------------- DARK MODE TOGGLE ----------------
+document.querySelector('.dark-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    const btn = document.querySelector('.dark-toggle');
+    btn.textContent = document.body.classList.contains('dark') ? "☀️" : "🌙";
+});
+
+
+// Load theme on page start
+window.onload = () => {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+    }
+};
